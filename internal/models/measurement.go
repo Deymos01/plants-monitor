152d45021510615b995bbf89d6347451f873503d
Deymos01 <essenion@gmail.com>
2026-04-25 18:31:30 +0300

    feat: added models description

--- internal/models/measurement.go
+++ internal/models/measurement.go
@@ -1,1 +1,25 @@
 package models
+
+import "time"
+
+type Measurement struct {
+	ID             int64     `json:"id"`
+	DeviceID       string    `json:"device_id"`
+	SoilRaw        float64   `json:"soil_raw"`
+	SoilVoltage    float64   `json:"soil_voltage"`
+	SoilPercent    int       `json:"soil_percent"`
+	LightRaw       float64   `json:"light_raw"`
+	LightVoltage   float64   `json:"light_voltage"`
+	BatteryVoltage *float64  `json:"battery_voltage,omitempty"`
+	CreatedAt      time.Time `json:"created_at"`
+}
+
+type CreateMeasurementRequest struct {
+	DeviceID       string   `json:"device_id"`
+	SoilRaw        float64  `json:"soil_raw"`
+	SoilVoltage    float64  `json:"soil_voltage"`
+	SoilPercent    int      `json:"soil_percent"`
+	LightRaw       float64  `json:"light_raw"`
+	LightVoltage   float64  `json:"light_voltage"`
+	BatteryVoltage *float64 `json:"battery_voltage,omitempty"`
+}
